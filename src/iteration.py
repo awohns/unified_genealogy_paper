da5ab5f33ee4c194bfa7db2b621262669815bd4d
awohns <awohns@gmail.com>
2020-05-22 18:09:35 +0100

    iteration script

--- src/iteration.py
+++ src/iteration.py
@@ -1,10 +1,13 @@
 import argparse
+import numpy as np
+
 import msprime
 import tsinfer
 import tskit
-import numpy as np
+import tsdate
 
 import utility as util
+import constants
 
 
 def iter_infer(input_modern_samples_fn, output_fn, Ne, mutation_rate):
@@ -12,6 +15,28 @@ def iter_infer(input_modern_samples_fn, output_fn, Ne, mutation_rate):
     Function to infer tree sequences from modern and ancient samples.
     """
     samples = tsinfer.load(input_modern_samples_fn)
+    # Remove any mutations only found in ancients
+    samples_pruned = remove_ancient_mutations(samples, output_fn)
+
+    # Remove ancients and infer tree sequence from only modern samples
+    modern_samples = make_modern_samples(samples_pruned, output_fn)
+    # Infer the ts based on sample data
+    inferred_ts = tsinfer.infer(modern_samples)
+    inferred_ts = inferred_ts.simplify()
+    inferred_ts.dump(output_fn + ".tsinferred.trees")
+
+    # tsdate runs
+    tsdated_ts = tsdate.date(inferred_ts, Ne, mutation_rate)
+
+    # Constrain and reinfer
+    samples_constrained = get_ancient_constraints(samples_pruned, modern_samples, tsdated_ts)
+    iter_infer = tsinfer.infer(samples_constrained).simplify()
+    iter_infer.dump(output_fn + ".iter.tsinferred.trees")
+    iter_dated_ts = tsdate.date(iter_infer, Ne, mutation_rate)
+    iter_dated_ts.dump(output_fn + "iter.tsdated.trees")
+
+
+def make_modern_samples(samples, output_fn):
     # Remove ancients and infer tree sequence from only modern samples
     with tsinfer.SampleData(
         path=output_fn + ".modern.samples",
@@ -27,7 +52,7 @@ def iter_infer(input_modern_samples_fn, output_fn, Ne, mutation_rate):
                 except:
                     raise ValueError("Population must have field 'name'")
         pop_metadata = samples.populations_metadata[:]
-        for indiv in modern_samples.individuals():
+        for indiv in samples.individuals():
             if indiv.time == 0:
                 samples_bool = samples.samples_individual[:] == indiv.id
                 modern_samples.add_individual(
@@ -43,45 +68,98 @@ def iter_infer(input_modern_samples_fn, output_fn, Ne, mutation_rate):
                 modern_samples.add_site(
                     var.site.position, var.genotypes[modern_samples_bool], var.alleles
                 )
-
-    # Infer the ts based on sample data
-    inferred_ts, tsinfer_cpu, tsinfer_memory = tsinfer.infer(modern_samples)
-    inferred_ts = inferred_ts.simplify()
-    inferred_ts.dump(output_fn + ".tsinferred.trees")
-
-    # tsdate runs
-    tsdate_dates = evaluation.run_tsdate_get_dates(inferred_ts, Ne, mutation_rate)
-
-    # Constrain and reinfer
-    samples_constrained = get_ancient_constraints(samples)
-    iter_infer = tsinfer.infer(samples_constrained).simplify()
-    iter_infer.dump(output_fn + ".iter.tsinferred.trees")
-    iter_dated_ts = evaluation.run_tsdate(iter_infer, Ne, mutation_rate)
-    iter_dated_ts.dump(output_fn + "iter.tsdated.trees")
+    return modern_samples
 
 
-def get_ancient_constraints(sampledata, dated_ts):
+def get_ancient_constraints(sampledata, modern_sampledata, dated_ts):
+    modern_sites_bool = np.isin(
+        modern_sampledata.sites_position[:], sampledata.sites_position[:]
+    )
+    ancient_sites_bool = np.isin(
+        sampledata.sites_position[:], modern_sampledata.sites_position[:]
+    )
     ancient_samples_bool = sampledata.individuals_time[:] != 0
     ancient_samples_age = sampledata.individuals_time[:][ancient_samples_bool]
-    genotypes = sampledata.sites_genotypes[:][:, ancient_samples_bool]
+    genotypes = sampledata.sites_genotypes[:]
+    genotypes = genotypes[ancient_sites_bool][:, ancient_samples_bool]
     ancient_sites = np.any(genotypes == 1, axis=1)
     mut_ages, _ = util.get_mut_ages(dated_ts, dated_ts.tables.nodes.time[:])
-    # positions = sampledata.sites_position[:][np.any(genotypes == 1, axis=1)]
     lower_bound = np.array(
-        [
-            np.max(ancient_samples_age[geno == 1])
-            for pos, geno in genotypes[ancient_sites]
-        ]
+        [np.max(ancient_samples_age[geno == 1]) for geno in genotypes[ancient_sites]]
     )
     # Find the maximum of the estimated tsdate age and the ancient constraints for each mutation
     constrained_ages = np.maximum(
-        mut_ages[ancient_sites] * constants.GENERATION_TIME, lower_bound
+        mut_ages[modern_sites_bool][ancient_sites] * constants.GENERATION_TIME,
+        lower_bound,
     )
     mut_ages[ancient_sites] = constrained_ages
-    sample_data_copy = samples.copy()
+    sample_data_copy = sampledata.copy()
     sample_data_copy.sites_time[:] = mut_ages
     sample_data_copy.finalise()
     return sample_data_copy
+
+
+def remove_ancient_mutations(samples, output_fn):
+    """
+    Remove mutations which only appear in ancient samples 
+    """
+    modern_samples_bool = samples.individuals_time[:] == 0
+    with tsinfer.SampleData(
+        path=output_fn + ".modern.samples",
+        sequence_length=samples.sequence_length,
+        num_flush_threads=2,
+    ) as samples_pruned:
+        for pop in samples.populations_metadata[:]:
+            samples_pruned.add_population(pop)
+        pop_metadata = samples.populations_metadata[:]
+        for indiv in samples.individuals():
+            samples_bool = samples.samples_individual[:] == indiv.id
+            population_id = samples.samples_population[:][samples_bool]
+            # All population labels for this individual should be the same
+            assert np.all(population_id == population_id[0])
+            samples_pruned.add_individual(
+                metadata=indiv.metadata,
+                time=indiv.time,
+                population=population_id[0], 
+                ploidy=np.sum(samples_bool),
+            )
+        for var in samples.variants():
+            if np.any(var.genotypes[modern_samples_bool] == 1):
+                samples_pruned.add_site(
+                    var.site.position, var.genotypes, var.alleles
+                )
+    return samples_pruned
+
+def make_modern_samples(samples, output_fn):
+    # Remove ancients and infer tree sequence from only modern samples
+    with tsinfer.SampleData(
+        path=output_fn + ".modern.samples",
+        sequence_length=samples.sequence_length,
+        num_flush_threads=2,
+    ) as modern_samples:
+        for pop in samples.populations_metadata[:]:
+            pop_id = modern_samples.add_population(pop)
+        for indiv in samples.individuals():
+            if indiv.time == 0:
+                samples_bool = samples.samples_individual[:] == indiv.id
+                population_id = samples.samples_population[:][samples_bool]
+                # All population labels for this individual should be the same
+                assert np.all(population_id == population_id[0])
+                modern_samples.add_individual(
+                    metadata=indiv.metadata,
+                    time=indiv.time,
+                    population=population_id[0], 
+                    ploidy=np.sum(samples_bool),
+                )
+
+        modern_samples_bool = samples.individuals_time[:] == 0
+        for var in samples.variants():
+            if np.any(var.genotypes[modern_samples_bool] == 1):
+                modern_samples.add_site(
+                    var.site.position, var.genotypes[modern_samples_bool], var.alleles
+                )
+    return modern_samples
+
 
 
 def main():
