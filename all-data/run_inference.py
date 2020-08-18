92796daf305c38f61ba031434c3107aeae0bcbdd
awohns <awohns@gmail.com>
2020-08-18 18:48:20 +0100

    new ancient pipeline

--- all-data/run_inference.py
+++ all-data/run_inference.py
@@ -3,6 +3,7 @@ import argparse
 import collections
 import re
 import time
+import logging
 
 import tskit
 import msprime
@@ -10,10 +11,12 @@ import numpy as np
 import tsinfer
 import stdpopsim
 
+logger = logging.getLogger(__name__)
+
 Params = collections.namedtuple(
     "Params",
-    "sample_file, ma_mis_rate, ms_mis_rate, cutoff_exponent, precision, "
-    "genetic_map, num_threads")
+    "sample_file, ancestors_file, ma_mis_rate, ms_mis_rate, cutoff_exponent, precision, "
+    "genetic_map, ancestors_ts, modern_samples_match, ancient_ancestors, num_threads")
 
 Results = collections.namedtuple(
     "Results",
@@ -31,12 +34,13 @@ def run(params):
     extra_params =  dict(num_threads=params.num_threads)
     if params.cutoff_exponent is not None:
         extra_params['cutoff_power'] = params.cutoff_exponent
-    anc = tsinfer.generate_ancestors(
-        samples,
-        progress_monitor=tsinfer.cli.ProgressMonitor(1, 1, 0, 0, 0),
-        **extra_params,
-    )
-    print(f"GA done (cutoff exponent: {params.cutoff_exponent}")
+    anc = tsinfer.load(params.ancestors_file)
+#    anc = tsinfer.generate_ancestors(
+#        samples,
+#        progress_monitor=tsinfer.cli.ProgressMonitor(1, 1, 0, 0, 0),
+#        **extra_params,
+#    )
+#    print(f"GA done (cutoff exponent: {params.cutoff_exponent}")
     inference_pos = anc.sites_position[:]
     if chr_map is not None:
         inference_distances = physical_to_genetic(chr_map, inference_pos)
@@ -49,8 +53,16 @@ def run(params):
 	    ([0.0], d/anc.sequence_length))
 
     av_rho = np.quantile(rho, 0.5)
-    ma_mis = av_rho * params.ma_mis_rate
-    ms_mis = av_rho * params.ms_mis_rate
+    if params.ma_mis_rate is not None:
+        ma_mis_rate = params.ma_mis_rate
+    else:
+        ma_mis_rate = 0.1
+    if params.ms_mis_rate is not None:
+        ms_mis_rate = params.ms_mis_rate
+    else:
+        ms_mis_rate = 0.1
+    ma_mis = av_rho * ma_mis_rate
+    ms_mis = av_rho * ms_mis_rate
     if params.precision is None:
         # Smallest nonzero recombination rate
         min_rho = int(np.ceil(-np.min(np.log10(rho[rho > 0]))))
@@ -59,12 +71,6 @@ def run(params):
         precision = max(min_rho, av_min) + 3
     else:
         precision = params.precision
-    print(
-        f"Starting match_ancestors, {params.ma_mis_rate} {params.ms_mis_rate}",
-        f"with av rho {av_rho:.5g}",
-        f"(mean {np.mean(rho):.4g}, median {np.quantile(rho, 0.5):.4g}, ",
-        f"nonzero min {np.min(rho[rho > 0]):.4g}, ",
-        f"2.5% quantile {np.quantile(rho, 0.025):.4g}) precision {precision}")
     prefix = None
     if params.sample_file is not None:
         assert params.sample_file.endswith(".samples")
@@ -81,19 +87,19 @@ def run(params):
         precision=precision,
     )
 
-    if prefix is not None:
-        anc_copy = anc.copy(prefix + ".ancestors")
-        anc_copy.finalise()
-    if np.any(samples.individuals_time[:] > 0) and not np.any(samples.sites_time[:] == tsinfer.constants.TIME_UNSPECIFIED):
-        inferred_anc_ts = match_ancestors_including_noncontemporanous_samples(
-            samples,
-            anc,
-            mismatch_rate=ma_mis,
-            create_sample_nodes=True,
-            progress_monitor=tsinfer.cli.ProgressMonitor(1, 0, 1, 0, 0),
-            **extra_params,
-        )
-    else:
+#    if prefix is not None:
+#        anc_copy = anc.copy(prefix + ".ancestors")
+#        anc_copy.finalise()
+    if params.ma_mis_rate is not None:
+        print(
+            f"Starting match_ancestors, {params.ma_mis_rate} {params.ms_mis_rate}",
+            f"with av rho {av_rho:.5g}",
+            f"(mean {np.mean(rho):.4g}, median {np.quantile(rho, 0.5):.4g}, ",
+            f"nonzero min {np.min(rho[rho > 0]):.4g}, ",
+            f"2.5% quantile {np.quantile(rho, 0.025):.4g}) precision {precision}")
+
+        if params.ancient_ancestors:
+            anc = anc.insert_proxy_samples(samples, allow_mutation=True)
         inferred_anc_ts = tsinfer.match_ancestors(
             samples,
             anc,
@@ -101,23 +107,36 @@ def run(params):
             progress_monitor=tsinfer.cli.ProgressMonitor(1, 0, 1, 0, 0),
             **extra_params,
         )
-    inferred_anc_ts.dump(path=prefix + ".atrees")
-    print(f"MA done (ma_mis:{ma_mis}")
-    inferred_ts = tsinfer.match_samples(
-        samples,
-        inferred_anc_ts,
-        mismatch_rate=ms_mis,
-        simplify=False,
-        progress_monitor=tsinfer.cli.ProgressMonitor(1, 0, 0, 0, 1),
-        **extra_params,
-    )
-    process_time = time.process_time() - start_time
-    ts_path = prefix + ".trees"
-    inferred_ts.dump(path=ts_path)
-    print(f"MS done: ms_mis rate = {ms_mis})")
-    simplified_inferred_ts = inferred_ts.simplify()  # Remove unary nodes
-    ts_path = prefix + ".simplified.trees"
-    simplified_inferred_ts.dump(path=ts_path)
+        inferred_anc_ts.dump(path=prefix + ".atrees")
+        print(f"MA done (ma_mis:{ma_mis}")
+    if params.ancestors_ts is not None:
+        inferred_anc_ts = tskit.load(params.ancestors_ts)
+    if params.ms_mis_rate is not None and params.modern_samples_match:
+        samples = samples.subset(np.where(samples.individuals_time[:] == 0)[0])
+        prefix = prefix + ".modern"
+    if params.ms_mis_rate is not None and params.ancient_ancestors:
+        prefix = prefix + ".historic"
+        force_sample_times = True
+    else:
+        force_sample_times = False
+
+    if params.ms_mis_rate is not None:
+        inferred_ts = tsinfer.match_samples(
+            samples,
+            inferred_anc_ts,
+            mismatch_rate=ms_mis,
+            simplify=False,
+            progress_monitor=tsinfer.cli.ProgressMonitor(1, 0, 0, 0, 1),
+            force_sample_times=force_sample_times,
+            **extra_params,
+        )
+        process_time = time.process_time() - start_time
+        ts_path = prefix + ".nosimplify.trees"
+        inferred_ts.dump(path=ts_path)
+        print(f"MS done: ms_mis rate = {ms_mis})")
+        simplified_inferred_ts = inferred_ts.simplify(filter_sites=False)  # Remove unary nodes
+        ts_path = prefix + ".trees"
+        simplified_inferred_ts.dump(path=ts_path)
     
 def physical_to_genetic(recombination_map, input_physical_positions):
     map_pos = recombination_map.get_positions()
@@ -155,133 +174,6 @@ def get_genetic_map(filename, genetic_map=None):
 
     return chr_map 
 
-def match_ancestors_including_noncontemporanous_samples(
-    sample_data, ancestor_data, *, create_sample_nodes=None, **kwargs,
-):
-    """
-    Like match_ancestors, but also include as ancestral nodes those samples in the
-    sample_data file from individuals whose time is > 0. These "non-contemporaneous"
-    samples are added into the list of ancestors before running match_ancestors. This
-    is only possible if the times of mutations used for inference (the site times in the
-    ancestor_data file) have been explicitly set by the user, e.g. by using the
-    ``time`` parameter of ``SampleData.add_sites()``, or by setting ``use_times=True`` if
-    creating a sample data file via ``tsinfer.SampleData.from_tree_sequence()``.
-    If ``create_sample_nodes`` is True, the inserted ancestors will be flagged with
-    tsinfer.NODE_IS_TRUE_SAMPLE_ANCESTOR, and the individuals associated with the
-    non-contemporaneous samples will be present in the resulting ancestors tree sequence.
-    This will create internal sample nodes in the final tree sequence produced by
-    match_samples. If create_sample_nodes is False, the inserted ancestors are treated as
-    *proxies* for the true samples (i.e. close, unrecombined relatives of the
-    true samples) and the nodes will not be contained within an individual in the final
-    tree sequence.
-    Returns an ancestors tree sequence suitable for input into match_samples. If
-    ``create_sample_nodes`` is True, you probably want to exclude any noncontemporaneous
-    samples during the match_samples phase, otherwise they risk being duplicated in the
-    final tree sequence.
-    """
-
-    # First find the sites from the samples file used in the built ancestors
-    sites_used = np.isin(sample_data.sites_position[:], ancestor_data.sites_position[:])
-    sites_used_time = sample_data.sites_time[:][sites_used]
-    if np.any(sites_used_time == tsinfer.constants.TIME_UNSPECIFIED):
-        raise ValueError(
-            "Noncontemporaneous samples can only be ancestors if site times are present"
-        )
-    individuals_population = sample_data.individuals_population[:]
-
-    # Find the non-contemporaneous (nc) samples and group them by individuals_time
-    samples_individual = sample_data.samples_individual[:]
-    nc_individuals = sample_data.individuals_time[:] > 0
-    nc_samples = np.where(np.isin(samples_individual, np.where(nc_individuals)[0]))[0]
-    samples_by_timepoint = collections.defaultdict(list)
-    for s in nc_samples:
-        timepoint = sample_data.individuals_time[samples_individual[s]]
-        samples_by_timepoint[timepoint].append(s)
-
-    logger.info("Creating new ancestors file with added ancestors")
-    ancestor_to_sample_map = {}
-    inferred_ancestors = ancestor_data.ancestors()
-    current_anc = next(inferred_ancestors)
-    sample_data_subset = sample_data.subset(sites=np.where(sites_used)[0])
-    with tsinfer.formats.AncestorData(sample_data=sample_data_subset) as new_data:
-        for insert_time, sample_ids in sorted(samples_by_timepoint.items()):
-            while current_anc is not None and current_anc.time > insert_time:
-                new_data.add_ancestor(**attr.asdict(current_anc, filter=exclude_id))
-                current_anc = next(inferred_ancestors, None)
-            for s in sample_ids:
-                logger.debug(f"Adding sample {s} as an ancestor")
-                haplotype = sample_data.sites_genotypes[:, s][sites_used]
-                if np.any(sites_used_time[haplotype > 0] < insert_time):
-                    bad = sites_used_time[haplotype > 0] < insert_time
-                    bad_pos = sample_data.sites_position[:][bad]
-                    raise ValueError(
-                        "Non-contemporanous ancestor is older than its derived mutations"
-                        + f" at sites {bad_pos}"
-                    )
-                anc_id = new_data.add_ancestor(
-                    start=0,
-                    end=ancestor_data.num_sites,
-                    time=insert_time,
-                    focal_sites=[],
-                    haplotype=haplotype,
-                )
-                ancestor_to_sample_map[anc_id] = s
-    logger.info("Matching ancestors including noncontemporaneous samples")
-    ancestor_tables = tsinfer.match_ancestors(
-        sample_data, new_data, **kwargs
-    ).dump_tables()
-    nodes_flags = ancestor_tables.nodes.flags
-
-    if create_sample_nodes is True:
-        logger.info("Adding associated individuals and populations")
-
-        ancestor_individuals = ancestor_tables.individuals
-        ancestor_populations = ancestor_tables.populations
-        assert ancestor_individuals.num_rows == 0
-        assert ancestor_populations.num_rows == 0
-
-        # Insert individuals into ancestors tree sequence
-        old_to_new_indiv = {}
-        for i in np.unique(samples_individual[nc_samples]):
-            old_to_new_indiv[i] = ancestor_individuals.add_row(
-                flags=0,
-                location=sample_data.individual(i).location,
-                metadata=json.dumps(sample_data.individual(i).metadata).encode(),
-            )
-        # Insert populations into ancestors tree sequence
-        old_to_new_pop = {}
-        for p in np.unique(individuals_population[nc_individuals]):
-            old_to_new_pop[p] = ancestor_populations.add_row(
-                metadata=json.dumps(sample_data.population(p).metadata).encode()
-            )
-
-        logger.info("Setting node information for true sample ancestors")
-        nodes = ancestor_tables.nodes
-        nodes_individual = nodes.individual
-        nodes_population = nodes.population
-        for anc_id, samp_id in ancestor_to_sample_map.items():
-            i = samples_individual[samp_id]
-            # The ids of the ancestors in the a_ts should correspond to the ones in the
-            # ancestors file, as all the pc ancestors are at the end but we check anyway
-            assert anc_id == json.loads(nodes[anc_id].metadata)["ancestor_data_id"]
-            nodes_individual[anc_id] = old_to_new_indiv[i]
-            nodes_population[anc_id] = old_to_new_pop[individuals_population[i]]
-            nodes_flags[anc_id] = np.bitwise_or(
-                nodes_flags[anc_id], NODE_IS_TRUE_SAMPLE_ANCESTOR,
-            )
-        ancestor_tables.nodes.individual = nodes_individual
-        ancestor_tables.nodes.population = nodes_population
-
-    else:
-        logger.info("Setting node information for proxy sample ancestors")
-        for anc_id, _ in ancestor_to_sample_map.items():
-            nodes_flags[anc_id] = np.bitwise_or(
-                nodes_flags[anc_id], NODE_IS_PROXY_SAMPLE_ANCESTOR,
-            )
-
-    ancestor_tables.nodes.flags = nodes_flags
-    return ancestor_tables.tree_sequence()
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
@@ -291,12 +183,14 @@ if __name__ == "__main__":
             " where 'NN' is a number, assume this is a human samples file and use the"
             " appropriate recombination map from the thousand genomes project, otherwise"
             " use the physical distance between sites.")
+    parser.add_argument("ancestors_file", default=None,
+        help="A tsinfer ancestors file ending in '.ancestors'") 
     # The _mrate parameter defaults set from analysis ot 1000G, see
     # https://github.com/tskit-dev/tsinfer/issues/263#issuecomment-639060101
-    parser.add_argument("-A", "--mismatch_ancestors", type=float, default=0.1,
+    parser.add_argument("-A", "--mismatch_ancestors", type=float, default=None,
         help="The mismatch probability in the match ancestors phase,"
             " as a fraction of the median recombination probability between sites")
-    parser.add_argument("-S", "--mismatch_samples", type=float, default=0.1,
+    parser.add_argument("-S", "--mismatch_samples", type=float, default=None,
         help="The mismatch probability in the match samples phase,"
             " as a fraction of the median recombination probability between sites")
     parser.add_argument("-p", "--precision", type=int, default=None,
@@ -313,15 +207,28 @@ if __name__ == "__main__":
     parser.add_argument("-x", "--cutoff_exponent", default=None, type=float,
         help="The value, x to be used as the exponenent of the freq, to shorten"
             "ancestor building")
+    parser.add_argument("--ancestors-ts", default=None,
+            help="An inferred ancestors tree sequence to match samples against")
+    parser.add_argument("--ancient-ancestors", action="store_true",
+            help="If true, ancients are added as ancestors in match_ancestors")
+    parser.add_argument("--modern-samples-match", action="store_true",
+            help="If true, only match modern samples to ancestors tree sequence.")
+
     args = parser.parse_args()
     prefix = args.sample_file[:-len(".samples")]
+    if args.ancestors_ts is not None and args.mismatch_ancestors is not None:
+        raise ValueError("Cannot match ancestors when providing ancestors ts")
     params = Params(
         args.sample_file,
+        args.ancestors_file,
         args.mismatch_ancestors,
         args.mismatch_samples,
         args.cutoff_exponent,
         args.precision,
         args.genetic_map,
+        args.ancestors_ts,
+        args.modern_samples_match,
+        args.ancient_ancestors,
         args.num_threads,
     )
     run(params)
