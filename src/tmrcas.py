5b2cb24aeca0eb1244fa139c095985829dc8a037
Yan Wong <yan.wong@bdi.ox.ac.uk>
2020-09-22 17:14:40 +0100

    Alternative method for calculating tMRCAs

--- src/tmrcas.py
+++ src/tmrcas.py
@@ -7,70 +7,130 @@ import itertools
 from tqdm import tqdm
 import collections
 
-ts = tskit.load("merged_hgdp_1kg_sgdp_high_cov_ancients_chr20.dated.binned.historic.trees")
+def get_pairwise_tmrca_pops(
+    ts, hist_nbins=30, hist_min_gens=1000, return_full_data=False
+):
+    """
+    hist_nbins determines the number of bins used to save the histogram data,
+    hist_min_gens gives a lower cutoff for the histogram bins, as there is usually
+    very little in the lowest (logged) bins
+    if return_full_data is True, also return the full dataset of weights (which may be
+    huge, as it is ~ num_unique_times * n_pops * n_pops /2
+    """
+    deleted_trees = [tree.index for tree in ts.trees() if tree.parent(0) == -1]  
+    node_ages = np.zeros_like(ts.tables.nodes.time[:])
+    metadata = ts.tables.nodes.metadata[:]
+    metadata_offset = ts.tables.nodes.metadata_offset[:]
+    try:
+        for index, met in enumerate(tskit.unpack_bytes(metadata, metadata_offset)):
+            if index not in ts.samples():
+                try:
+                    # Get unconstrained node age if available
+                    node_ages[index] = json.loads(met.decode())["mn"]
+                except json.decoder.JSONDecodeError:
+                    raise ValueError(
+                            "Tree Sequence must be dated to use unconstrained=True")
+        print("Using tsdate unconstrained node times")
+    except KeyError:
+        print("Using standard ts node times")
+        node_ages[:] = ts.tables.nodes.time[:]
+    unique_times, time_index = np.unique(node_ages, return_inverse=True)
+    with np.errstate(divide='ignore'):
+        log_unique_times = np.log(unique_times)
 
-node_ages = ts.tables.nodes.time[:]
-deleted_trees = [tree.index for tree in ts.trees() if tree.parent(0) == -1]  
-
-metadata = ts.tables.nodes.metadata[:]
-metadata_offset = ts.tables.nodes.metadata_offset[:]
-for index, met in enumerate(tskit.unpack_bytes(metadata, metadata_offset)):
-    if index not in ts.samples():
-        try:
-            node_ages[index] = json.loads(met.decode())["mn"]
-        except json.decoder.JSONDecodeError:
-            raise ValueError(
-                    "Tree Sequence must be dated to use unconstrained=True")
-pop_nodes = ts.tables.nodes.population[ts.samples()]
-pop_nodes = [np.where(pop_nodes == pop.id)[0] for pop in ts.populations()]
-rand_nodes = list()
-for nodes in pop_nodes:
-    if len(nodes) > 20:
-        rand_nodes.append(np.random.choice(nodes, 20, replace=False))
-    else:
-        rand_nodes.append(nodes)
-
-
-def get_pairwise_tmrca_pops(ts):
+    # Make a random selection of up to 10 samples from each population
+    np.random.seed(123)
+    pop_nodes = ts.tables.nodes.population[ts.samples()]
+    pop_nodes = [np.where(pop_nodes == pop.id)[0] for pop in ts.populations()]
+    rand_nodes = list()
+    for nodes in pop_nodes:
+        if len(nodes) > 20:
+            rand_nodes.append(np.random.choice(nodes, 20, replace=False))
+        else:
+            rand_nodes.append(nodes)
+    
+    # Make all combinations of populations
     pop_names = [json.loads(pop.metadata)["name"] for pop in ts.populations()]
-    if input == "1kg_sgdp_hgdp":
-        pop_name_suffixes = pop_names[0:26]
-        for pop in pop_names[26:156]:
-            pop_name_suffixes.append(pop + "_SGDP")
-        for pop in pop_names[156:]:
-            pop_name_suffixes.append(pop + "_HGDP")
-        pop_names = pop_name_suffixes
     tmrca_df = pd.DataFrame(columns=pop_names, index=pop_names)
-    pop_rows = list()
-    combos = list(itertools.combinations_with_replacement(np.arange(0, ts.num_populations),2))
-    weights = np.array([tree.span for tree in ts.trees() if tree.index not in deleted_trees])
-    with multiprocessing.Pool(processes=10) as pool: 
-        for avg_tmrca, combo in tqdm(pool.imap_unordered(get_avg_tmrca, combos), total=len(combos)):
-            tmrca_df.loc[pop_names[combo[0]], pop_names[combo[1]]] = np.exp(np.average(np.log(avg_tmrca), weights=weights))
-    return tmrca_df
+    combos = itertools.combinations_with_replacement(np.arange(0, ts.num_populations), 2)
+    combo_map = {c: i for i, c in enumerate(combos)}
+    func_params = zip(
+        combo_map.keys(),
+        itertools.repeat(time_index),
+        itertools.repeat(rand_nodes),
+        itertools.repeat(ts),
+        itertools.repeat(deleted_trees),
+    )
+    data = np.zeros((len(combo_map), len(unique_times)), dtype=np.float)
+    with multiprocessing.Pool(processes=2) as pool: 
+        for tmrca_weight, combo in tqdm(
+            pool.imap_unordered(get_tmrca_weights, func_params), total=len(combo_map)
+        ):
+            popA = pop_names[combo[0]]
+            popB = pop_names[combo[1]]
+            keep = (tmrca_weight != 0)  # Deal with log_unique_times[0] == -inf
+            mean_log_age = np.sum(log_unique_times[keep] * tmrca_weight[keep])
+            mean_log_age /= np.sum(tmrca_weight) # Normalise
+            tmrca_df.loc[popA, popB] = np.exp(mean_log_age)
+            data[combo_map[combo], :] = tmrca_weight
+    bins, hist_data = make_histogram_data(
+        log_unique_times, data, hist_nbins, hist_min_gens)
+    if return_full_data:
+        return tmrca_df, bins, hist_data, data
+    else:
+        return tmrca_df, bins, hist_data
 
-def get_avg_tmrca(combo):
-    avg_tmrca = []
+def make_histogram_data(log_unique_times, data, hist_nbins, hist_min_gens):
+    """
+    NB: this can also be called on the (saved) full data matrix, if histograms need
+    re-calculating with different bin widths etc.
+    """
+    av_weight = np.mean(data, axis=0)
+    keep == (av_weight != 0)
+    #Make common breaks for histograms
+    _, bins = np.histogram(
+        log_unique_times[keep],
+        weights=av_weight[keep],
+        bins=hist_nbins,
+        range=[np.log(hist_min_gens), max(log_unique_times)],
+        density=True)
+    hist_data = np.zeros((len(combo_map), hist_nbins), dtype=np.float32)
+    for i, row in enumerate(data):
+        hist_data[i, :] = np.histogram(
+            log_unique_times[keep],
+            weights=row[keep],
+            bins=bins,
+            density=True,
+        )
+    return bins, hist_data
+
+def get_tmrca_weights(params):
+    combo, time_index, rand_nodes, ts, deleted_trees = params
     pop_0 = combo[0]
     pop_1 = combo[1]
+    num_unique_times = max(time_index) + 1
     pop_0_nodes = rand_nodes[pop_0]
     if pop_0 != pop_1:
         pop_1_nodes = rand_nodes[pop_1]
         node_combos = [(x, y) for x in pop_0_nodes for y in pop_1_nodes]
     elif pop_0 == pop_1:
         node_combos = list(itertools.combinations(pop_0_nodes, 2))
-    avg_tmrca = np.zeros((ts.num_trees - len(deleted_trees), len(node_combos)))
+    # Return the weights 
+    tmrca_weight = np.zeros(num_unique_times, dtype=np.float)
 
-    t_index = 0
     for tree in ts.trees(): 
         if tree.index not in deleted_trees:
-            tree_tmrcas = []
             for index, (node_0, node_1) in enumerate(node_combos):
-                avg_tmrca[t_index, index] = node_ages[tree.mrca(node_0, node_1)]
-            t_index += 1
-    avg_tmrca = np.exp(np.mean(np.log(avg_tmrca), axis=1))
-    return avg_tmrca, combo
+                tmrca_weight[time_index[tree.mrca(node_0, node_1)]] += tree.span
+    return tmrca_weight, combo
 
-
-df = get_pairwise_tmrca_pops(ts)
-df.to_csv("all-data/merged_hgdp_1kg_sgdp_high_cov_ancients_chr20.dated.binned.historic.tmrcas")
+if __name__ == '__main__':
+    file = "all-data/merged_hgdp_1kg_sgdp_high_cov_ancients_chr20.dated.binned.historic"
+    ts = tskit.load(file + ".trees")
+    
+    tmrca_df, bins, hist_data = get_pairwise_tmrca_pops(ts)
+    outfile = file + ".tmrcas"
+    print(f"Writing mean MRCAs to {outfile}.csv")
+    tmrca_df.to_csv(outfile + ".csv")
+    print(f"Writing bins and MRCA histogram distributions to {outfile}.npz")
+    np.savez_compressed(outfile + ".npz", bins=bins, hist_data=hist_data)
