634b5859e05ccd9f21b919f78ff45bd1aadaf1e2
Yan Wong <yan.wong@bdi.ox.ac.uk>
2020-12-30 15:46:21 +0000

    Also output SVG file
    This is useful for post-processing, such as changing contour line labels.

--- src/plot.py
+++ src/plot.py
@@ -343,6 +343,9 @@ class Figure(object):
             plt.savefig(
                 "figures/{}.png".format(figure_name), bbox_inches="tight", dpi=400
             )
+            plt.savefig(
+                "figures/{}.svg".format(figure_name), bbox_inches="tight", format="svg"
+            )
             plt.close()
 
     def error_label(self, error, label_for_no_error="No genotyping error"):
